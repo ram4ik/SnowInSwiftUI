579e5dfdcf6122fc2b94dbe5f674a0765fcbd521
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-23 09:00:02 +0200

    Initial Commit

---
+++ SnowInSwiftUI/SnowInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowInSwiftUIApp.swift
+//  SnowInSwiftUI
+//
+//  Created by ramil on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SnowInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
