ef42231671b3aae984b3413cd162bc373a300f20
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-23 09:20:05 +0200

    init

--- SnowInSwiftUI/ContentView.swift
+++ SnowInSwiftUI/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SnowView()
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -18,4 +18,40 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct SnowView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> some UIView {
+        let screenSize = UIScreen.main.bounds
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+        view.layer.masksToBounds = true
+        
+        let emitterLayer = CAEmitterLayer()
+        emitterLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        emitterLayer.emitterShape = .circle
+        emitterLayer.emitterPosition = CGPoint(x: screenSize.width / 2, y: -100)
+        
+        emitterLayer.emitterMode = .surface
+        emitterLayer.renderMode = .oldestLast
+        
+        let cell = CAEmitterCell()
+        cell.birthRate = 100
+        cell.lifetime = 10
+        cell.velocity = 100
+        cell.scale = 0.03
+        cell.emissionRange = CGFloat.pi
+        
+        cell.contents = UIImage(named: "snowflake")?.cgImage
+        
+        emitterLayer.emitterCells = [cell]
+        view.layer.addSublayer(emitterLayer)
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
 }
